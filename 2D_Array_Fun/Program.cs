4013d46ff6f8533f0617d0fe3b3704ab34aab40f
YociusProductions <90871690+StardustIsArt@users.noreply.github.com>
2025-10-08 13:10:44 -0500

    Adding user input for columns and rows in the grid. Also creating the 2D array itself with the user inputs.

--- 2D_Array_Fun/Program.cs
+++ 2D_Array_Fun/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Hello! Let's make a 2D Grid!");
+        Console.WriteLine("How many columns would you like in your grid?");
+        int columns = int.Parse(Console.ReadLine());
+        Console.WriteLine("How many rows would you like in your grid?");
+        int rows = int.Parse(Console.ReadLine());
+        int [,] grid = new int [columns, rows];
+        
     }
 }
